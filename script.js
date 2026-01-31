e4aa9658d3ac4f1f9fb48709553ecff2c3af3cc1
ysooch0819 <59263935+ysooch0819@users.noreply.github.com>
2026-01-31 17:48:43 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function sayHello() {
   alert("눌렀다!");
 }
+
+const postList = document.getElementById("post-list");
+
+posts.forEach((post) => {
+  const li = document.createElement("li");
+  li.textContent = post.title + " - " + post.summary;
+  postList.appendChild(li);
+});
